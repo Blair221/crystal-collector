564085bb050a8479a37d4359b153c245fd6d007c
Blair Brownlie <bbrownli@asu.edu>
2019-03-08 20:25:28 -0700

    set up crystal images and getting the hang of their click functions

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    var wins = 0;
+    var losses = 0;
+    var currentTotal = 0;
+    var randomNumber = Math.floor(Math.random() * 121) + 19;
+    var crystalValue = Math.floor(Math.random()*13) +1;
+    console.log(crystalValue);
+    console.log(randomNumber);
+    $('.crystal').on('click', function() {
+        alert('this crystal is worth: ' + crystalValue);
+    });
+    
+});
