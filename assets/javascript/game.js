d065dcc0fd0834193f01dc577159c60c66449cbd
Blair Brownlie <bbrownli@asu.edu>
2019-03-11 17:32:31 -0700

    finalizing my html and bootstrap elements before I get down to the nitty gritty javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
     var losses = 0;
     var currentTotal = 0;
     var randomNumber = Math.floor(Math.random() * 121) + 19;
-    var crystalValue = Math.floor(Math.random()*13) +1;
+    var crystalValue = Math.floor(Math.random() * 13) + 1;
     console.log(crystalValue);
     console.log(randomNumber);
-    $('.crystal').on('click', function() {
-        alert('this crystal is worth: ' + crystalValue);
-    });
     
+    $('.crystal').on('click', function (){
+        alert('This crystal is worth ' + crystalValue);
+    })
 });
