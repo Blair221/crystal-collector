1e852e8b435022ad04d9d496ecc316091de43ee4
Blair Brownlie <bbrownli@asu.edu>
2019-03-11 17:49:23 -0700

    set-up my start button for the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(function() {
     var wins = 0;
     var losses = 0;
     var currentTotal = 0;
@@ -6,8 +6,12 @@ $(document).ready(function () {
     var crystalValue = Math.floor(Math.random() * 13) + 1;
     console.log(crystalValue);
     console.log(randomNumber);
+  $("#start-button").on("click", function() {
     
-    $('.crystal').on('click', function (){
-        alert('This crystal is worth ' + crystalValue);
-    })
+
+    $(".crystal").on("click", function() {
+      alert("This crystal is worth " + crystalValue);
+    });
+
+  });
 });
